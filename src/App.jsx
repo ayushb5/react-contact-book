ac9b14fb0bed23f7e3e0f3bbcb91670bae1339c7
Ayush <179284688+ayushb5@users.noreply.github.com>
2026-04-18 21:28:39 +0530

    Used props, localStorage

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,16 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Navbar from './Components/Navbar'
 import Card from './Components/Card'
 
 function App() {
+  const [contactData, setContactData] = useState(() => {
+    const data = JSON.parse(localStorage.getItem("contacts"));
+    return data || [];
+  });
   return (
     <>
-      <Navbar />
-      <Card />
+      <Navbar contactData={contactData} setContactData={setContactData} />
+      <Card contactData={contactData} />
     </>
   )
 }
