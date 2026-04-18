67ba008ea2e027ba8e5d8e7e8feb677f5d9777c7
Ayush <179284688+ayushb5@users.noreply.github.com>
2026-04-18 21:27:34 +0530

    Added some working logic for Navbar

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,6 +1,55 @@
-import React from 'react'
+import React, { useState } from 'react'
 
-function Navbar() {
+function Navbar({ contactData, setContactData }) {
+
+    const [formData, setFormData] = useState({
+        name: "",
+        passion: "",
+        skills: "",
+        avatar: "",
+        email: ""
+    })
+
+    const handleChange = (e) => {
+        setFormData({ ...formData, [e.target.id]: e.target.value })
+    }
+
+    const handleAddContact = () => {
+        if (!formData.name || !formData.passion || !formData.skills || !formData.avatar || !formData.email) {
+            alert("All fields are required.");
+            return;
+        }
+        const exists = contactData.some(contact => contact.email === formData.email);
+        if (exists) {
+            alert("Contact with this email already exists!");
+            return;
+        }
+        const newContact = {
+            id: Date.now(),
+            ...formData
+        };
+
+        const updatedContacts = [...contactData, newContact];
+        setContactData(updatedContacts);
+
+        localStorage.setItem("contacts", JSON.stringify(updatedContacts));
+
+        setFormData({
+            name: "",
+            passion: "",
+            skills: "",
+            avatar: "",
+            email: ""
+        })
+
+        const modal = document.getElementById('addContact');
+        const modalInstance = window.bootstrap.Modal.getInstance(modal);
+        modalInstance.hide();
+
+        alert("Contact Added successfully.")
+    }
+
+
     return (
         <>
             <nav className='navbar navbar-expand-lg bg-light'>
@@ -26,29 +75,29 @@ function Navbar() {
                         </div>
                         <div className="modal-body">
                             <div className="mb-3">
-                                <label htmlFor="personName" className='form-label'>Name</label>
-                                <input type="text" className='form-control' placeholder='Enter name...' id="personName" />
+                                <label htmlFor="name" className='form-label'>Name</label>
+                                <input type="text" className='form-control' value={formData.name} placeholder='Enter name...' id="name" onChange={handleChange} />
                             </div>
                             <div className="mb-3">
                                 <label htmlFor="passion" className='form-label'>Passion</label>
-                                <input type="text" className='form-control' placeholder='Enter passion...' id='passion' />
+                                <input type="text" className='form-control' value={formData.passion} placeholder='Enter passion...' id='passion' onChange={handleChange} />
                             </div>
                             <div className="mb-3">
                                 <label htmlFor="skills" className='form-label'>Skills</label>
-                                <input type="text" className='form-control' placeholder='Enter any 2 skills...' id='skills' />
+                                <input type="text" className='form-control' value={formData.skills} placeholder='Enter any 2 skills...' id='skills' onChange={handleChange} />
                             </div>
                             <div className="mb-3">
                                 <label htmlFor="avatar" className='form-label'>Avatar</label>
-                                <input type="text" className='form-control' placeholder='Give Image (ID size avatar) url...' id='avatar' />
+                                <input type="text" className='form-control' value={formData.avatar} placeholder='Give Image (ID size avatar) url...' id='avatar' onChange={handleChange} />
                             </div>
                             <div className="mb-3">
                                 <label htmlFor="email" className='form-label'>Email Id</label>
-                                <input type="text" className='form-control' placeholder='Enter email id...' id='email' />
+                                <input type="text" className='form-control' value={formData.email} placeholder='Enter email id...' id='email' onChange={handleChange} />
                             </div>
 
                             <div className="d-flex justify-content-end gap-2">
                                 <button className='btn btn-danger' data-bs-dismiss="modal">Cancel</button>
-                                <button className='btn btn-success'>Add Contact</button>
+                                <button type='button' className='btn btn-success' onClick={handleAddContact}>Add Contact</button>
                             </div>
                         </div>
                     </div>
